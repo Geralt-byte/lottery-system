1c4eeff5d26cf8f8bf2e840102e8495b3ba916d2
ffjtsvjku <ffjtsvjku@noreply.gitcode.com>
2026-03-20 04:12:52 +0800

    feat: 一阶段部署

--- big-market-infrastructure/src/main/java/com/xjtu/infrastructure/persistent/dao/IStrategyRuleDao.java
+++ big-market-infrastructure/src/main/java/com/xjtu/infrastructure/persistent/dao/IStrategyRuleDao.java
@@ -2,13 +2,14 @@ package com.xjtu.infrastructure.persistent.dao;
 
 import com.xjtu.infrastructure.persistent.po.StrategyRule;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 /*策略规则表Dao*/
 
 @Mapper
 public interface IStrategyRuleDao {
     /**根据策略id和规则模型查找策略规则*/
-    StrategyRule queryStrategyRule(Long strategyId, String ruleModel);
+    StrategyRule queryStrategyRule(@Param("strategyId") Long strategyId,@Param("ruleModel") String ruleModel);
 
     /**根据策略id,规则模型,奖品id查询规则值*/
     String queryStrategyRuleValue(StrategyRule strategyRule);
