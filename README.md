528b143100b9259ed11d9b74f67bf3e0a78802e3
ffjtsvjku <ffjtsvjku@noreply.gitcode.com>
2026-03-22 03:20:30 +0800

    feat: 用户参与抽奖活动库表设计

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
 /*****************************************************************/
 
 
-20260318-mlei-raffle-controller分支完成了controller接口<br>
-1.数据库表中树相关表有调整,增加树种类和树节点、边种类<br>
-2.big-market-types/src/main/java/com/xjtu/types中增加若干所需字段或实体类<br>
-3.StrategyAward、RaffleAwardEntity、RaffleFactorEntity、StrategyAwardEntity调整了若干字段<br>
-4.仓储层增加根据策略和奖品查询奖品信息的方法<br>
-5.配置文件配合修改<br>
-6.新增big-market-api模块，用于向外部开放接口<br>
-7.big-market-trigger/src/main/java/com/xjtu/trigger/http/RaffleController对外部接口进行实现<br>
+20260321-mlei-table-activity分支完成了用户参与抽奖活动库表设计<br>
+1.数据库表中增加五张表，增加对应实体类<br>
 /*****************************************************************/
---
+++ big-market-infrastructure/src/main/java/com/xjtu/infrastructure/persistent/po/RaffleActivityAccount.java
@@ -0,0 +1,36 @@
+package com.xjtu.infrastructure.persistent.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author mlei@xjtu
+ * @description 抽奖活动账户表
+ * @create 2026/3/22 02:50
+ */
+@Data
+public class RaffleActivityAccount {
+    /**自增ID*/
+    private Long id;
+    /**用户ID*/
+    private String userId;
+    /**活动ID*/
+    private Long activityId;
+    /**总次数*/
+    private Integer totalCount;
+    /**总次数-剩余*/
+    private Integer totalCountSurplus;
+    /**日次数*/
+    private Integer dayCount;
+    /**日次数-剩余*/
+    private Integer dayCountSurplus;
+    /**月次数*/
+    private Integer monthCount;
+    /**月次数-剩余*/
+    private Integer monthCountSurplus;
+    /**创建时间*/
+    private Date createTime;
+    /**更新时间*/
+    private Date updateTime;
+}
