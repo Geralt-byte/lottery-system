79679cc6a2f1ab8bd3d0ac3881a08f2863c4ab8f
ffjtsvjku <ffjtsvjku@noreply.gitcode.com>
2026-04-05 23:18:25 +0800

    feat: 引入动态线程池组件

--- big-market-app/src/main/java/com/xjtu/config/RedisClientConfig.java
+++ big-market-app/src/main/java/com/xjtu/config/RedisClientConfig.java
@@ -17,6 +17,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.io.IOException;
 
@@ -28,6 +29,7 @@ import java.io.IOException;
 public class RedisClientConfig {
 
     @Bean("redissonClient")
+    @Primary
     public RedissonClient redissonClient(ConfigurableApplicationContext applicationContext, RedisClientConfigProperties properties) {
         Config config = new Config();
         // 根据需要可以设定编解码器；https://github.com/redisson/redisson/wiki/4.-%E6%95%B0%E6%8D%AE%E5%BA%8F%E5%88%97%E5%8C%96
